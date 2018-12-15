3606f6942aef018e55d1b1ad2c2644df1964967e
Brenna Switzer <brenna.switzer@emergeinteractive.com>
2018-12-15 11:25:59 -0800

    Catchup previous work

---
+++ day_10/src/main/java/com/brennaswitzer/aoc/Sky.java
@@ -0,0 +1,105 @@
+package com.brennaswitzer.aoc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+// TODO : create a snapshot of state so that we don't mutate our stars as we move them
+// TODO: only generate the part of the sky that you need to look at. it's not absolute
+
+public class Sky {
+
+  List<Point> stars = new ArrayList<>();
+  int offset = 10;
+  int[][] sky;
+
+  Sky(int size) { }
+
+  int getOffsetX(int x) {
+    return x + offset;
+  }
+
+  int getOffsetY(int y) {
+    return y + offset;
+  }
+
+  void setStar(int x, int y, int vx, int vy) {
+    Point star = new Point(x, y, vx, vy);
+    stars.add(star);
+  }
+
+  void draw(int s) {
+    // need our calculate function back to we can create a new arraylist for every second in the sky rather than mutation
+    // either that or every second needs to just be "one"
+    // the size of the grid needs the size of the stars we need to draw, so we need the parse the output of the data points
+    sky = new int[10][10];
+    for (int x = 0; x < sky.length; x++) {
+      for (int y = 0; y < sky.length; y++) {
+        sky[x][y] = 0;
+      }
+    }
+
+    // set new state based on which second it is
+    for (Point p : stars) {
+      p.setPoint(p.getXpos(), p.getYpos());
+    }
+
+    // assign those values to the grid
+    for (Point p : stars) {
+      sky[p.getX()][p.getY()] = 1;
+    }
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+
+    for (int x = 0; x < sky.length; x++) {
+      for (int y = 0; y < sky.length; y++) {
+        if (sky[x][y] == 1) {
+          sb.append("#");
+        } else {
+          sb.append(".");
+        }
+      }
+      sb.append("\n");
+    }
+    return sb.toString();
+  }
+
+  static public class Point {
+    int x;
+    int y;
+    int vx;
+    int vy;
+
+    Point(int x, int y, int vx, int vy) {
+      this.x = x;
+      this.y = y;
+      this.vx = vx;
+      this.vy = vy;
+    }
+
+    int getXpos() {
+      return this.x + this.vx;
+    }
+
+    int getYpos() {
+      return this.y + this.vy;
+    }
+
+    int getX() {
+      return x;
+    }
+
+    int getY() {
+      return y;
+    }
+
+    void setPoint(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+}
--- day_10/src/test/java/com/brennaswitzer/aoc/MainTest.java
+++ day_10/src/test/java/com/brennaswitzer/aoc/MainTest.java
@@ -6,5 +6,31 @@ import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
+  Sky one = new Sky(30);
+  Sky two = new Sky(10);
+
+  @Before
+  public void setup() {
+    one.setStar(3, 9, 1, -2);
+  }
+
+  @Test
+  public void testPointMovement() {
+    Sky.Point test = new Sky.Point(3, 9, 1, -2);
+    assertEquals(4, test.getXpos());
+    assertEquals(7, test.getYpos());
+  }
+
+  @Test
+  public void testSeeSky() {
+    two.setStar(0, 0, 1, 1);
+    two.setStar(1, 1, 1, 1);
+
+    for (int i = 0; i < 4; i++) {
+      two.draw(i);
+      System.out.println(two);
+    }
+  }
+
 
 }
