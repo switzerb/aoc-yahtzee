b81f3bb02114316e004e29a10f1cb76f40be2a3c
Brenna Switzer <brenna.switzer@gmail.com>
2018-12-17 20:05:35 -0800

    Day 12, part one and attempts at part two

---
+++ day_12/src/main/java/com/brennaswitzer/aoc/Cavern.java
@@ -0,0 +1,78 @@
+package com.brennaswitzer.aoc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cavern {
+    
+    String initial;
+    List<Rule> rules = new ArrayList<>();
+    
+    Cavern(String initial) {
+        this.initial = initial;
+    }
+    
+    void addRule(String rule) {
+        String[] split = rule.split("=>");
+        Rule r = new Rule(split[0].trim(), split[1].trim());
+        rules.add(r);
+    }
+    
+    String willGrow(String pattern) {
+        for (Rule r : rules) {
+            if (pattern.equals(r.in)) {
+                return r.out;
+            }
+        }
+        return ".";
+    }
+    
+    int getSumOfPots(int gens) {
+        int sum = 0;
+        int center = 0;
+        
+        String prev = initial;
+        String last = "";
+    
+        for (int e = 0; e < gens; e++) {
+            prev = "....." + prev + "...";
+            String next = "";
+            center += 3;
+        
+            for (int i = 2; i < prev.length() - 2; i++) {
+                // need to send the pot, plus two previous and two after to willGrow function
+                String pattern = prev.substring(i - 2, i + 3);
+                next += willGrow(pattern);
+            }
+            prev = next;
+            if(e == gens -1){
+                last = next;
+            }
+        }
+    
+        for (int i = 0; i < last.length(); i++) {
+            if(last.charAt(i) == '#') {
+                sum += i - center;
+            }
+        }
+        return sum;
+    }
+    
+    long getHugeEvolution() {
+        long sum = 0L;
+        sum += getSumOfPots(400);
+
+        sum += (50_000_000_000L - 400) * 6300;
+        return sum;
+    }
+    
+    public static class Rule {
+        String in;
+        String out;
+        
+        Rule(String in, String out) {
+            this.in = in;
+            this.out = out;
+        }
+    }
+}
---
+++ day_12/src/test/java/com/brennaswitzer/aoc/MainTest.java
@@ -0,0 +1,57 @@
+package com.brennaswitzer.aoc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MainTest {
+    
+    Cavern cave;
+    
+    @Before
+    public void setup() throws IOException {
+        ClassLoader cl = Main.class.getClassLoader();
+        InputStream in = cl.getResourceAsStream("test_input.txt");
+        BufferedReader r = new BufferedReader(new InputStreamReader(in));
+        
+        List<String> input = new ArrayList<>();
+        
+        String initial = r.readLine().substring(15);
+        cave = new Cavern(initial);
+        
+        while (true) {
+            String line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            input.add(line);
+        }
+        
+        for (String l : input) {
+            if (l.length() > 0) {
+                cave.addRule(l);
+            }
+        }
+        
+    }
+    
+    @Test
+    public void testEvolution() {
+        long sum = cave.getHugeEvolution();
+        assertEquals(1374, sum);
+    }
+    
+    @Test
+    public void testSumOfPots() {
+        assertEquals(325, cave.getSumOfPots(20));
+    }
+    
+}
