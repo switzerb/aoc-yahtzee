69c7766b86e0dd5a1eec32873541fe0eddd34aa2
Brenna Switzer <brenna.switzer@gmail.com>
2018-12-17 20:33:56 -0800

    Add skeleton day 13

--- day_13/src/test/java/com/brennaswitzer/aoc/MainTest.java
+++ day_13/src/test/java/com/brennaswitzer/aoc/MainTest.java
@@ -1,10 +1,21 @@
 package com.brennaswitzer.aoc;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
+    
+    @Before
+    public void setUp() {
+        int[][] track = new int[15][15];
+        track[0][0] = '/';
+        track[1][0] = '-';
+        track[2][0] = '-';
+        track[3][0] = '-';
+        track[4][0] = '\\';
+    }
 
 
 }
