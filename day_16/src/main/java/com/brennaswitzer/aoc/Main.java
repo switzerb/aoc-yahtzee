271863c859b30b12e172e2391dbdc9d6e843ad96
Brenna Switzer <brenna.switzer@gmail.com>
2018-12-20 21:06:35 -0800

    Add parsing and input

--- day_16/src/main/java/com/brennaswitzer/aoc/Main.java
+++ day_16/src/main/java/com/brennaswitzer/aoc/Main.java
@@ -14,16 +14,67 @@ public class Main {
         BufferedReader r = new BufferedReader(new InputStreamReader(in));
         
         List<String> input = new ArrayList<>();
+        Register instructions = new Register();
         
         while (true) {
             String line = r.readLine();
             if (line == null) {
                 break;
             }
-            input.add(line);
+            if (line.length() > 0) {
+                input.add(line);
+            }
         }
-
-        Register instructions = new Register();
+        
+        int count = 0;
+        int id = 0;
+        int A = 0;
+        int B = 0;
+        int C = 0;
+        int[] before = {0, 0, 0, 0};
+        int[] after = {0, 0, 0, 0};
+        
+        for (String l : input) {
+            count++;
+            
+            String[] split = l.split(" ");
+            
+            if (split[0].trim().equals("Before:")) {
+                before = parseBefore(split);
+            } else if (split[0].trim().equals("After:")) {
+                after = parseAfter(split);
+            } else {
+                id = Integer.parseInt(split[0].trim());
+                A = Integer.parseInt(split[1].trim());
+                B = Integer.parseInt(split[2].trim());
+                C = Integer.parseInt(split[3].trim());
+            }
+            
+            if (count % 3 == 0) {
+                instructions.addOpcode(id, A, B, C, before, after);
+            }
+        }
+        
+        instructions.buildPotentials();
+        System.out.println(instructions.countThrees());
+        
     }
     
+    public static int[] parseBefore(String[] split) {
+        return new int[]{
+                Integer.parseInt(split[1].substring(1, 2)),
+                Integer.parseInt(split[2].substring(0, 1)),
+                Integer.parseInt(split[3].substring(0, 1)),
+                Integer.parseInt(split[4].substring(0, 1))
+        };
+    }
+    
+    public static int[] parseAfter(String[] split) {
+        return new int[]{
+                Integer.parseInt(split[2].substring(1, 2)),
+                Integer.parseInt(split[3].substring(0, 1)),
+                Integer.parseInt(split[4].substring(0, 1)),
+                Integer.parseInt(split[5].substring(0, 1))
+        };
+    }
 }
