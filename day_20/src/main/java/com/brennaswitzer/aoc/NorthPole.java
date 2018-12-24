ce82cb1c48618d087daea032b5f565cea415fe1d
Brenna Switzer <brenna.switzer@gmail.com>
2018-12-24 12:19:02 -0800

    Add in the delegated Point class so I can use rows and columns

--- day_20/src/main/java/com/brennaswitzer/aoc/NorthPole.java
+++ day_20/src/main/java/com/brennaswitzer/aoc/NorthPole.java
@@ -1,6 +1,5 @@
 package com.brennaswitzer.aoc;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -8,7 +7,7 @@ import java.util.Set;
 public class NorthPole {
     
     HashMap<Point, Character> facility = new HashMap<>();
-    Point current = new Point();
+    Point current = new Point(0,0);
     
     NorthPole() {
         start();
@@ -16,52 +15,33 @@ public class NorthPole {
     
     void start() {
         Point start = new Point(0, 0);
+        current.setLocation(start);
         facility.put(start, 'X');
     }
     
     void mapRoute(String route) {
         String[] dirs = route.split("");
         for (String d : dirs) {
+            System.out.println(current);
             if (d.equals("W")) {
                 mapWest();
             }
-            if(d.equals("E")) {
+            if (d.equals("E")) {
                 mapEast();
             }
-            if(d.equals("N")){
+            if (d.equals("N")) {
                 mapNorth();
             }
         }
     }
     
     void mapWest() {
-        Point tail = current.getLocation();
-        tail.setLocation(tail.getX() + -1,  tail.getY() + 0);
-        facility.put(tail, '|');
-        
-        tail.setLocation(tail.getX() + -1,  tail.getY() + 0);
-        facility.put(tail, '.');
-        
-        System.out.println(tail);
     }
     
     void mapEast() {
-        Point tail = current.getLocation();
-        tail.setLocation(tail.getX() + 1,  tail.getY() + 0);
-        facility.put(tail, '|');
-    
-        tail.setLocation(tail.getX() + 1,  tail.getY() + 0);
-        facility.put(tail, '.');
     }
     
     void mapNorth() {
-        Point tail = current.getLocation();
-        tail.setLocation(tail.getX() + 0,  tail.getY() + -1);
-        facility.put(tail, '|');
-    
-        tail.setLocation(tail.getX() + 0,  tail.getY() + -1);
-        facility.put(tail, '.');
-    
     }
     
     void mapSouth() {
@@ -72,11 +52,79 @@ public class NorthPole {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         Set<Map.Entry<Point, Character>> entries = facility.entrySet();
-        
-        for (Map.Entry<Point, Character> point : entries) {
-            sb.append(point.getValue());
-        }
+
+//        for (int i = -10; i < 10; i++) {
+//            sb.append("\n");
+//            for (int j = -10; j < 10; j++) {
+//                Point p = new Point(i, j);
+//                if (facility.containsKey(p)) {
+//                    System.out.println(facility.get(p));
+////                    sb.append(facility.get(p));
+//                } else {
+////                    sb.append("?");
+//                }
+//            }
+//        }
+
+//
+//        for (Map.Entry<Point, Character> point : entries) {
+//            sb.append(point.getValue());
+//        }
         return sb.toString();
     }
     
+    public class Point implements Comparable<Point> {
+        
+        private final java.awt.Point p;
+        
+        public Point(int row, int col) {
+            p = new java.awt.Point(col, row);
+        }
+        
+        public int getRow() {
+            return p.y;
+        }
+        
+        public int getCol() {
+            return p.x;
+        }
+        
+        public void setLocation(int row, int col) {
+            this.p.y = row;
+            this.p.x = col;
+        }
+        
+        public void setLocation(Point point) {
+            this.p.y = point.p.y;
+            this.p.x = point.p.x;
+        }
+        
+        
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Point)) return false;
+            Point point = (Point) o;
+            return p.equals(point.p);
+        }
+        
+        @Override
+        public int hashCode() {
+            return p.hashCode();
+        }
+        
+        @Override
+        public int compareTo(Point o) {
+            int c = p.y - o.p.y;
+            if (c != 0) return c;
+            return p.x - o.p.x;
+        }
+    
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[" + p.y + "," + p.x + "]");
+            return sb.toString();
+        }
+    }
 }
