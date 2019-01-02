1a2009e9cd6d0aec51f07e921bbe0e97f27b4b21
switzerb <brenna.switzer@gmail.com>
2019-01-01 20:29:36 -0800

    Add Turn and Direction enums to house animation data

---
+++ day_13/src/main/java/com/brennaswitzer/aoc/Direction.java
@@ -0,0 +1,5 @@
+package com.brennaswitzer.aoc;
+
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT
+}
---
+++ day_13/src/main/java/com/brennaswitzer/aoc/Turn.java
@@ -0,0 +1,5 @@
+package com.brennaswitzer.aoc;
+
+public enum Turn {
+    STRAIGHT, RIGHT, LEFT
+}
