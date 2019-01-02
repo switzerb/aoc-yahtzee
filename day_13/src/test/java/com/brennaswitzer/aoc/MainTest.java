06fb0afbd4bda7617b657b35cc3eb7c1e70d834b
switzerb <brenna.switzer@gmail.com>
2019-01-01 18:07:26 -0800

    Move Histogram to utils and make generic

--- day_13/src/test/java/com/brennaswitzer/aoc/MainTest.java
+++ day_13/src/test/java/com/brennaswitzer/aoc/MainTest.java
@@ -6,15 +6,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
-    
+
     @Before
     public void setUp() {
-        int[][] track = new int[15][15];
-        track[0][0] = '/';
-        track[1][0] = '-';
-        track[2][0] = '-';
-        track[3][0] = '-';
-        track[4][0] = '\\';
+        char[][] track = {
+                {'/', '-', '-', '-', '\\'},
+                {'|', ' ', ' ', ' ', '|', ' '}
+        };
     }
 
 
