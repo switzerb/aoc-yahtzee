32115237c5d7dfcbc9632cb0fdfb01ecb48236ce
switzerb <brenna.switzer@gmail.com>
2019-01-02 20:38:00 -0800

    Create collision and test for position

--- day_13/src/main/java/com/brennaswitzer/aoc/Cart.java
+++ day_13/src/main/java/com/brennaswitzer/aoc/Cart.java
@@ -10,6 +10,7 @@ public class Cart {
     Point position;
     Direction direction;
     Turn nextTurn = Turn.LEFT;
+    boolean wrecked;
 
     Cart(int x, int y, Direction dir) {
         position = new Point(x, y);
@@ -69,18 +70,26 @@ public class Cart {
 
     @Override
     public String toString() {
-        if (direction == Direction.EAST) {
-            return ">";
+        if(wrecked) {
+            return "X";
+        } else {
+            if (direction == Direction.EAST) {
+                return ">";
+            }
+            if (direction == Direction.WEST) {
+                return "<";
+            }
+            if (direction == Direction.NORTH) {
+                return "^";
+            }
+            if (direction == Direction.SOUTH) {
+                return "v";
+            }
+            return " ";
         }
-        if (direction == Direction.WEST) {
-            return "<";
-        }
-        if (direction == Direction.NORTH) {
-            return "^";
-        }
-        if (direction == Direction.SOUTH) {
-            return "v";
-        }
-        return " ";
+    }
+
+    public void wreck() {
+        wrecked = true;
     }
 }
