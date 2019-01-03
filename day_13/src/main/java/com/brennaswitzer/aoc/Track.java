39c2b4bc61d98ce01407abfbba34e6221ec07d0b
switzerb <brenna.switzer@gmail.com>
2019-01-02 21:18:30 -0800

    Remove the 10,000 printed lines

--- day_13/src/main/java/com/brennaswitzer/aoc/Track.java
+++ day_13/src/main/java/com/brennaswitzer/aoc/Track.java
@@ -97,8 +97,6 @@ public class Track {
                     stop = true;
                 }
             }
-            System.out.println(this);
-            System.out.println('\n');
             if(stop) break;
         }
 
