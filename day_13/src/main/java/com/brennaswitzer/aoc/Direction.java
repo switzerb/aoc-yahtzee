c69130cde96d4453f1d365f98fd2d9a15d4b1dfa
switzerb <brenna.switzer@gmail.com>
2019-01-02 19:31:23 -0800

    Conversation between track and cart to determine direction

--- day_13/src/main/java/com/brennaswitzer/aoc/Direction.java
+++ day_13/src/main/java/com/brennaswitzer/aoc/Direction.java
@@ -1,5 +1,27 @@
 package com.brennaswitzer.aoc;
 
+import java.awt.*;
+
 public enum Direction {
-    NORTH, SOUTH, WEST, EAST
+    NORTH(-1, 0), SOUTH(1, 0), EAST(0, 1), WEST(0, -1);
+
+    int row_delta, col_delta;
+
+    Direction(int r, int c) {
+        this.row_delta = r;
+        this.col_delta = c;
+    }
+
+    Point straight(Point p) {
+        return new Point(p.x + col_delta, p.y + row_delta);
+    }
+
+    Point corner(Point p) {
+        if(this == EAST) {
+            return new Point(p.x + col_delta, p.y + row_delta);
+//        } else {
+//            return new Point(p.x + col_delta, p.y + row_delta);
+        }
+        throw new RuntimeException("Barney says fuck you.");
+    }
 }
